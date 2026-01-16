21a372da28530019d726ecf70d94b84070993db3
subho8680 <subhodipadhikari88@gmail.com>
2026-01-17 00:57:29 +0530

    fixed some bugs

--- src/ApiCall.jsx
+++ src/ApiCall.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = import.meta.env.VITE_API_URL || "https://blog-backend-zf94.onrender.com/api/v1";
+const API = "https://blog-backend-zf94.onrender.com/api/v1";
 export const regUser = async (formdata) => {
     try {
         const res = await axios.post(`${API}/user/register`, formdata, {
